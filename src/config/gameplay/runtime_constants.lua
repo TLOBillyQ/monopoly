2d6ea40288d656c22429b0ab5f184f0019e46756
覃远杰 <g23qyj@gmail.com>
2026-05-02 14:10:03 +0800

    revert(anim/robot): robot_speed 恢复 18.0

--- src/config/gameplay/runtime_constants.lua
+++ src/config/gameplay/runtime_constants.lua
@@ -30,7 +30,7 @@ local runtime_constants = {
 
   walk_speed = 13.0,
   speed_boost_modifier_key = 100000,
-  robot_speed = 9.0,
+  robot_speed = 18.0,
 
   vehicle_speed = 20.0,
   vehicle_accel = 20.0,
